faff756dbb169ded8b089a2094b5b4e8095701fe
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2024-09-20 16:33:42 +0200

    Add old tests

---
+++ tests/resources/test_resources.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+
+from careamics_napari.resources import *
+
+
+def test_gear():
+    assert Path(ICON_GEAR).exists()
+    assert type(ICON_GEAR) == str
+
+
+def test_github():
+    assert Path(ICON_GITHUB).exists()
+    assert type(ICON_GITHUB) == str
+
+
+def test_juglab():
+    assert Path(ICON_CAREAMICS).exists()
+    assert type(ICON_CAREAMICS) == str
+
+
+def test_tf():
+    assert Path(ICON_TF).exists()
+    assert type(ICON_TF) == str
