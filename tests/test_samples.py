54fbaec73639c287cea9a8340c224acc927f9d28
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2024-10-20 23:07:11 +0200

    remove test

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from careamics_napari.sample_data import n2n_sem_data, n2v_sem_data, u2os_care_data
+from careamics_napari.sample_data import n2n_sem_data, n2v_sem_data
 
 
 def test_n2v_sem_data():
@@ -11,11 +11,5 @@ def test_n2v_sem_data():
 
 def test_n2n_sem_data():
     data = n2n_sem_data()
-    assert len(data) == 2
-    assert all(isinstance(d, np.ndarray) for d, _ in data)
-
-
-def test_u2os_care_data():
-    data = u2os_care_data()
     assert len(data) == 2
     assert all(isinstance(d, np.ndarray) for d, _ in data)
