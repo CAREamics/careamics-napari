2fd688f4871a7f5dc27547d47dc00a630882aceb
jdeschamps <6367888+jdeschamps@users.noreply.github.com>
2024-10-20 23:11:46 +0200

    Remove dataset tests

--- tests/test_samples.py
+++ tests/test_samples.py
@@ -1,15 +1,10 @@
-import numpy as np
-
-from careamics_napari.sample_data import n2n_sem_data, n2v_sem_data
-
-
-def test_n2v_sem_data():
-    data = n2v_sem_data()
-    assert len(data) == 2
-    assert all(isinstance(d, np.ndarray) for d, _ in data)
+# def test_n2v_sem_data():
+#     data = n2v_sem_data()
+#     assert len(data) == 2
+#     assert all(isinstance(d, np.ndarray) for d, _ in data)
 
 
-def test_n2n_sem_data():
-    data = n2n_sem_data()
-    assert len(data) == 2
-    assert all(isinstance(d, np.ndarray) for d, _ in data)
+# def test_n2n_sem_data():
+#     data = n2n_sem_data()
+#     assert len(data) == 2
+#     assert all(isinstance(d, np.ndarray) for d, _ in data)
