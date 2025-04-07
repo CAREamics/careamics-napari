057742f052978ecc10a2b3b157b920fe93a2cb8e
Mehdi Seifi <mehdiseifi@gmail.com>
2025-04-07 17:05:00 +0200

    init prediction widget; added load model

--- src/careamics_napari/prediction_plugin.py
+++ src/careamics_napari/prediction_plugin.py
@@ -1,3 +1,324 @@
-"""CAREamics prediction plugin."""
+"""CAREamics prediction Qt widget."""
 
-# TODO
+from pathlib import Path
+from queue import Queue
+from typing import TYPE_CHECKING, Optional
+
+from qtpy.QtCore import Qt
+from qtpy.QtWidgets import (
+    QHBoxLayout, QStackedWidget, QVBoxLayout, QWidget,
+    QPushButton, QLineEdit, QFileDialog
+)
+from typing_extensions import Self
+
+from careamics_napari.signals import (
+    PredictionSignal,
+    PredictionState,
+    PredictionStatus,
+    PredictionUpdate,
+    PredictionUpdateType,
+    # SavingSignal,
+    # SavingState,
+    # SavingStatus,
+    # SavingUpdate,
+    # SavingUpdateType,
+    # TrainingSignal,
+    TrainingState,
+    # TrainingStatus,
+    # TrainUpdate,
+    # TrainUpdateType,
+)
+from careamics_napari.widgets import (
+    # AlgorithmSelectionWidget,
+    CAREamicsBanner,
+    # ConfigurationWidget,
+    PredictionWidget,
+    # SavingWidget,
+    ScrollWidgetWrapper,
+    # TrainDataWidget,
+    # TrainingWidget,
+    # TrainProgressWidget,
+    create_gpu_label,
+)
+# from careamics.config.support import SupportedAlgorithm
+from careamics_napari.workers import predict_worker
+from careamics_napari.utils.axes_utils import reshape_prediction
+from careamics import CAREamist
+# from careamics.config import Configuration
+from careamics.model_io import load_pretrained
+
+import numpy as np
+
+if TYPE_CHECKING:
+    import napari
+
+# at run time
+try:
+    import napari
+    import napari.utils.notifications as ntf
+
+except ImportError:
+    _has_napari = False
+else:
+    _has_napari = True
+
+
+class PredictionPluginWrapper(ScrollWidgetWrapper):
+    """Training plugin within a scrolling wrapper.
+
+    Parameters
+    ----------
+    napari_viewer : napari.Viewer or None, default=None
+        Napari viewer.
+    """
+
+    def __init__(self: Self, napari_viewer: Optional[napari.Viewer] = None) -> None:
+        """Initialize the plugin.
+
+        Parameters
+        ----------
+        napari_viewer : napari.Viewer or None, default=None
+            Napari viewer.
+        """
+        super().__init__(PredictionPlugin(napari_viewer))
+
+
+class PredictionPlugin(QWidget):
+    """CAREamics prediction plugin.
+
+    Parameters
+    ----------
+    napari_viewer : napari.Viewer or None, default=None
+        Napari viewer.
+    """
+
+    def __init__(
+        self: Self,
+        napari_viewer: Optional[napari.Viewer] = None,
+    ) -> None:
+        """Initialize the plugin.
+
+        Parameters
+        ----------
+        napari_viewer : napari.Viewer or None, default=None
+            Napari viewer.
+        """
+        super().__init__()
+        self.viewer = napari_viewer
+        self.careamist: Optional[CAREamist] = None
+
+        # create statuses, used to keep track of the threads statuses
+        self.pred_status = PredictionStatus()  # type: ignore
+
+        # create signals, used to hold the various parameters modified by the UI
+        self.pred_config_signal = PredictionSignal()
+
+        # create queues, used to communicate between the threads and the UI
+        self._prediction_queue: Queue = Queue(10)
+
+        self._init_ui()
+
+    def _init_ui(self) -> None:
+        """Assemble the widgets."""
+        # layout
+        self.setLayout(QVBoxLayout())
+        self.setMinimumWidth(200)
+
+        # add banner
+        self.layout().addWidget(
+            CAREamicsBanner(
+                title="CAREamics",
+                short_desc=("CAREamics UI for training denoising models."),
+            )
+        )
+        # gpu button
+        gpu_button = create_gpu_label()
+        gpu_button.setAlignment(Qt.AlignmentFlag.AlignRight)
+        gpu_button.setContentsMargins(0, 5, 0, 0)  # top margin
+        gpu_button.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        self.layout().addWidget(gpu_button)
+
+        # load model ui
+        load_button = QPushButton("Load Model...", self)
+        load_button.clicked.connect(self._select_model_checkpoint)
+        self.model_textbox = QLineEdit()
+        self.model_textbox.setReadOnly(True)
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.model_textbox)
+        hbox.addWidget(load_button)
+        hbox.setAlignment(Qt.AlignmentFlag.AlignLeft)
+        self.layout().addLayout(hbox)
+
+        # add prediction
+        self.prediction_widget = PredictionWidget(
+            train_status=None,
+            pred_status=self.pred_status,
+            train_signal=None,
+            pred_signal=self.pred_config_signal,
+        )
+        self.prediction_widget.setEnabled(False)
+        self.layout().addWidget(self.prediction_widget)
+
+        # changes from the training, prediction or saving state
+        self.pred_status.events.state.connect(self._prediction_state_changed)
+
+    def _select_model_checkpoint(self) -> None:
+        """Load a select CAREamics model."""
+        selected_file, _filter = QFileDialog.getOpenFileName(
+            self, "CAREamics", ".", "CAREamics Model(*.ckpt)"
+        )
+        if selected_file is not None and len(selected_file) > 0:
+            self.careamist = self._load_model(selected_file)
+            if self.careamist is None:
+                print(f"Error loading model: {selected_file}")
+                return
+            self.model_textbox.setText(selected_file)
+            self.prediction_widget.setEnabled(True)
+
+    def _load_model(self, model_path: str) -> Optional[CAREamist]:
+        """Load a CAREamics model.
+
+        Parameters
+        ----------
+        model_path : str
+            Path to the model checkpoint.
+
+        Returns
+        -------
+        careamist : CAREamist or None
+            CAREamist instance or None if the model could not be loaded.
+        """
+        try:
+            # model, config = load_pretrained(model_path)
+            careamist = CAREamist(model_path)
+            return careamist
+        except Exception as e:
+            print(f"Error loading model: {e}")
+            return None
+
+    def _prediction_state_changed(self, state: PredictionState) -> None:
+        """Handle prediction state changes.
+
+        Parameters
+        ----------
+        state : PredictionState
+            New state.
+        """
+        if state == PredictionState.PREDICTING:
+            self.pred_worker = predict_worker(
+                self.careamist, self.pred_config_signal, self._prediction_queue
+            )
+
+            self.pred_worker.yielded.connect(self._update_from_prediction)
+            self.pred_worker.start()
+
+        elif state == PredictionState.STOPPED:
+            # self.careamist.stop_prediction()
+            # TODO not existing yet
+            pass
+
+    def _update_from_prediction(self, update: PredictionUpdate) -> None:
+        """Update the signal from the prediction worker.
+
+        This method receives the updates from the prediction worker.
+
+        Parameters
+        ----------
+        update : PredictionUpdate
+            Update.
+        """
+        if update.type == PredictionUpdateType.DEBUG:
+            print(update.value)
+        elif update.type == PredictionUpdateType.EXCEPTION:
+            self.pred_status.state = PredictionState.CRASHED
+
+            # print exception without raising it
+            print(f"Error: {update.value}")
+
+            if _has_napari:
+                ntf.show_error(
+                    f"An error occurred during prediction: \n {update.value} \n"
+                    f"Note: if you get an error due to the sizes of "
+                    f"Tensors, try using tiling."
+                )
+
+        else:
+            if update.type == PredictionUpdateType.SAMPLE:
+                # add image to napari
+                # TODO keep scaling?
+                if self.viewer is not None:
+                    # value is either a numpy array or a list of numpy arrays,
+                    # with each sample/time-point as an element
+                    if isinstance(update.value, list):
+                        # combine all samples
+                        samples = np.concatenate(update.value, axis=0)
+                    else:
+                        samples = update.value
+
+                    # reshape the prediction to match the input axes
+                    samples = reshape_prediction(
+                        samples, self.train_config_signal.axes,
+                        self.pred_config_signal.is_3d
+                    )
+
+                    self.viewer.add_image(samples, name="Prediction")
+            else:
+                self.pred_status.update(update)
+
+    def _predict_button_clicked(self: Self) -> None:
+        """Run the prediction on the images."""
+        if self.pred_status is not None:
+            if (
+                self.pred_status.state == PredictionState.IDLE
+                or self.train_status.state == TrainingState.DONE
+                or self.pred_status.state == PredictionState.CRASHED
+            ):
+                self.pred_status.state = PredictionState.PREDICTING
+                self.predict_button.setEnabled(False)
+
+    def _set_data_from_algorithm(self, name: str) -> None:
+        """Update the data selection widget based on the algorithm.
+
+        Parameters
+        ----------
+        name : str
+            Algorithm name.
+        """
+        if (
+            name == SupportedAlgorithm.CARE.value
+            or name == SupportedAlgorithm.N2N.value
+        ):
+            self.data_stck.setCurrentIndex(1)
+        else:
+            self.data_stck.setCurrentIndex(0)
+
+    def closeEvent(self, event) -> None:
+        """Close the plugin.
+
+        Parameters
+        ----------
+        event : QCloseEvent
+            Close event.
+        """
+        super().closeEvent(event)
+        # TODO check training or prediction and stop it
+
+
+if __name__ == "__main__":
+    import faulthandler
+    import napari
+
+    log_file_fd = open("fault_log.txt", "a")
+    faulthandler.enable(log_file_fd)
+
+    # create a Viewer
+    viewer = napari.Viewer()
+
+    # add napari-n2v plugin
+    viewer.window.add_dock_widget(PredictionPluginWrapper(viewer))
+
+    # add image to napari
+    # viewer.add_image(data[0][0], name=data[0][1]['name'])
+
+    # start UI
+    napari.run()
